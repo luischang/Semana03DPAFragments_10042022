59c36f77c93c2423e2f11adbf0573e4c25876948
Luis Chang <luis.chang@iugo.com.uy>
2022-04-24 09:24:31 -0500

    Registro Fragment

--- app/src/main/java/dev/luischang/semana03dpafragments/ui/fragments/RegistroFragment.kt
+++ app/src/main/java/dev/luischang/semana03dpafragments/ui/fragments/RegistroFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import dev.luischang.semana03dpafragments.R
 
 class RegistroFragment : Fragment() {
@@ -12,7 +13,48 @@ class RegistroFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_registro, container, false)
+
+        val etFullName: EditText = view.findViewById(R.id.etFullName)
+        val etEmail: EditText = view.findViewById(R.id.etEmail)
+        val rgGender: RadioGroup = view.findViewById(R.id.rgGender)
+        val spnCountry: Spinner = view.findViewById(R.id.spnCountry)
+        val btnSave: Button = view.findViewById(R.id.btnSave)
+        var countryValue = ""
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.country_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter->
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+            spnCountry.adapter=adapter
+        }
+        //Obtener el valor del spinner
+        spnCountry.onItemSelectedListener= object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                countryValue = parent?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        btnSave.setOnClickListener {
+            var fullNameValue = etFullName.text
+            var emailValue = etEmail.text
+            val intSelectButton = rgGender.checkedRadioButtonId
+            val radioBUtton: RadioButton = view.findViewById(intSelectButton)
+            val genderValue = radioBUtton.text
+
+            Toast.makeText(requireContext(),"FullName : $fullNameValue", Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(),"Email : $emailValue", Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(),"Gender : $genderValue", Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(),"Country : $countryValue", Toast.LENGTH_LONG).show()
+
+        }
+
+
+        return view
     }
 }
